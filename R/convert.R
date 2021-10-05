84c1fe1bc3de4a009644d3e2e06275c829dd8d3d
‘NamanDhamejaBITSgit config --global user.email ‘h20210829@pilani.bits-pilani.ac.in <h20210829@pilani.bits-pilani.ac.in>
2021-10-06 00:28:34 +0530

    Update convert.R

---
+++ R/convert.R
@@ -0,0 +1,38 @@
+#' Title
+#'
+#' @param c
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+#'
+ctf<-function(c){
+  f<-c*1.8+32
+  return(f)
+}
+# fah to vel
+ftc<-function(f){
+  c<-(f-32)/1.8
+  return(c)
+}
+# cel to kelvin
+ctk<-function(c){
+  k<-c+273.15
+  return(k)
+}
+# kel to cel
+ktc<-function(k){
+  c<-k-273.15
+  return(c)
+}
+ftk<-function(f){
+  k<-(f-32)/1.8 + 273.15
+  return(k)
+}
+
+ktf<-function(k){
+  f<-k*1.8-459.67
+  return(f)
+}
